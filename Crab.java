e94832523bc0ddf4686d044fb7dae78c401e52db
jonathan <80464146+jonathan912@users.noreply.github.com>
2021-03-15 08:40:19 -0400

    Crab
    finished crab actions and added worm

--- Crab.java
+++ Crab.java
@@ -7,13 +7,25 @@ public class Crab extends Actor
 {
     public void act()
     {
+        if(isAtEdge())
+        {
+            turn(17);
+        } 
         
+        if (Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber (91) - 45);
+        }
+        move();
+        if (isTouching (Worm.class))
+        {
+            removeTouching (Worm.class);
+        }
     }
-    
-    public void move( )
-    {
-        move(5);
-    }
-}
-
-
+   public void move()
+   {
+       move(5);
+       
+    } 
+   
+} 
